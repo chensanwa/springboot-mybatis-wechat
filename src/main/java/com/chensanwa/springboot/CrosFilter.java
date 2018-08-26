a6ff3c8f4f7b72ada5ddc39f8028208552f36986
chensanwa <13008142306@163.com>
2018-08-26 22:09:44 +0800

    允许带Cookie跨域请求

--- src/main/java/com/chensanwa/springboot/CrosFilter.java
+++ src/main/java/com/chensanwa/springboot/CrosFilter.java
@@ -1,6 +1,9 @@
 package com.chensanwa.springboot;
 
+import org.springframework.util.StringUtils;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -19,10 +22,22 @@ public class CrosFilter implements javax.servlet.Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.addHeader("Access-Control-Allow-Origin","*");
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String origin = request.getHeader("Origin");
+        if(!StringUtils.isEmpty(origin)){
+            //设置响应头，允许跨域访问
+            //带cookie请求时，必须为全匹配，不能使用*
+            response.addHeader("Access-Control-Allow-Origin",origin);
+        }
+
         response.addHeader("Access-Control-Allow-Methods","*");
+        //允许JSON请求，并进行预检命令缓存
         response.addHeader("Access-Control-Allow-Headers","Content-Type");
         response.addHeader("Access-Control-Max-Age","3600");
+
+        //允许cookie
+        response.addHeader("Access-Control-Allow-Credentials","true");
         filterChain.doFilter(servletRequest,response);
     }
 
